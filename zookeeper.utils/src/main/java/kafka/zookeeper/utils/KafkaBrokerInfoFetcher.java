a8a5d803d84059348b97478bff7668a33c95f56b
ngvinay <vinay.ng.mys@gmail.com>
2016-07-10 10:45:03 -0400

    First commit

---
+++ zookeeper.utils/src/main/java/kafka/zookeeper/utils/KafkaBrokerInfoFetcher.java
@@ -0,0 +1,16 @@
+package kafka.zookeeper.utils;
+import org.apache.zookeeper.ZooKeeper;
+import java.util.*;
+
+public class KafkaBrokerInfoFetcher {
+
+    public static void main(String[] args) throws Exception {
+       // ZooKeeper zk = new ZooKeeper("localhost:2181", 10000, null);
+    	 ZooKeeper zk = new ZooKeeper("slave1:2181,slave2:2181,master:2181", 10000, null);
+        List<String> ids = zk.getChildren("/brokers/ids", false);
+        for (String id : ids) {
+            String brokerInfo = new String(zk.getData("/brokers/ids/" + id, false, null));
+            System.out.println(id + ": " + brokerInfo);
+        }
+    }
+}
